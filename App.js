d6b9f46e72e16318222ac9fb2d7223d543033c8c
vir-air-vent <alberto.gruning.zen@icloud.com>
2023-10-14 01:31:37 -0300

    refactor: add text style to DRY

--- App.js
+++ App.js
@@ -1,32 +1,32 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Text component N1</Text>
-      <Text 
-      style={{
-        backgroundColor:'silver',
-        borderRadius:10,
-        margin:50, 
-        borderWidth:3, 
-        borderColor:'silver', 
-        padding:16, 
-        color:'black'}}
-      >Text component N2
-      </Text> 
-      <Button title='Tap Here'/>
+      <Text style={styles.dummyText}>First Text</Text>
+      <Text style={styles.dummyText}>Text below</Text>
+      <Button title="Tap Here" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#307BAA',
-    color: 'navy',
-    alignItems: 'left',
-    justifyContent: 'center',
+    backgroundColor: "#307BAA",
+    color: "navy",
+    alignItems: "left",
+    justifyContent: "center",
   },
-})
-
+  dummyText: {
+    backgroundColor: "silver",
+    borderRadius: 10,
+    margin: 50,
+    borderWidth: 3,
+    borderColor: "silver",
+    padding: 16,
+    color: "black",
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+});
